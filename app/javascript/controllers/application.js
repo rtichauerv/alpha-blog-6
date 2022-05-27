97c79633cc698efca98ed6a87883aec1c67da6e9
rtichauerv <rtichauerv@gmail.com>
2022-05-26 20:58:18 -0400

    All CRUD and UI done, ready for deploy

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
 import { Application } from "@hotwired/stimulus"
-
+import Rails from "@rails/ujs"
+Rails.start()
 const application = Application.start()
 
 // Configure Stimulus development experience
+//=require jquery_ujs
+//=require jquery
 application.debug = false
 window.Stimulus   = application
+
 
 export { application }
