38b00040e4e28db1ffd625c7e3b20af584f6a638
rtichauerv <rtichauerv@gmail.com>
2022-05-29 03:52:23 -0400

    new users, activity_users and attending activity

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,6 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    
   end
 
   def index
@@ -35,10 +34,10 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def destroy
-    @article.destroy
-    redirect_to articles_path
-  end
+  # def destroy
+  #   @article.destroy
+  #   redirect_to articles_path
+  # end
 
   private
 
